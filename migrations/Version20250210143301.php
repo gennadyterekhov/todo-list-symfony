e72d643807965c9d720552a2eb48995cd978b97c
Gennady Terekhov <ter.ge@yandex.ru>
2025-02-10 18:41:06 +0400

    migration

---
+++ migrations/Version20250210143301.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250210143301 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("CREATE TABLE task (
+            id INT AUTO_INCREMENT NOT NULL,
+            title LONGTEXT NOT NULL,
+            description LONGTEXT NOT NULL,
+            status ENUM('new', 'in_progress', 'done') NOT NULL default 'new',
+            created_at DATETIME NOT NULL COMMENT '(DC2Type:datetimetz_immutable)' default CURRENT_TIMESTAMP,
+            updated_at DATETIME NOT NULL COMMENT '(DC2Type:datetimetz_immutable)' default CURRENT_TIMESTAMP,
+            PRIMARY KEY(id)
+        )
+        ");
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE task');
+    }
+}
